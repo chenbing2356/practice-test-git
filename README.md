59ed2dd9daa92fec4d526bdb0e968730afd42fb5
树叶 <@LAPTOP-G7EG95S7>
2022-11-23 17:29:36 +0800

    主干开发分支的功能完善

--- README.md
+++ README.md
@@ -4,4 +4,3 @@
 
 
 dev - 为主线开发分支
-feature_dev_zcb - 开发人员的开发分支
---
+++ develom_branch_func.py
@@ -0,0 +1,8 @@
+# 原始主线开发分支的代码
+
+import numpy as np
+import pandas as pd
+
+def baseline_analysis(data):
+    data_df = pd.DataFrame(data)
+    return data_df
